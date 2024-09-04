a503e0caa1536ab475a1abcf045c7be3a0f389df
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-09-04 23:20:20 +0100

    util.js: create global 'elem' function

---
+++ util.js
@@ -0,0 +1,19 @@
+(function(global){
+    "use strict";
+    global.elem = function elem(tag, attrs, children){
+        attrs = attrs || {};
+        children = children || [];
+        let e = document.createElement(tag);
+        Object.keys(attrs).forEach(
+            key => e.setAttribute(key, attrs[key])
+        );
+        children.forEach(function(child){
+            if (typeof child == 'string'){
+                child = document.createTextNode(child);
+            }
+            e.appendChild(child);
+        });
+        return e;
+    };
+
+})(window);
