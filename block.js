3e3f5373760012f52cd55a224ec57f10eec98375
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-09-05 12:41:46 +0100

    block.js: Add other block-related functionality

--- block.js
+++ block.js
@@ -22,3 +22,36 @@ function createBlock(name, value, contents){
 
   return item;
 }
+
+function blockContents(block){
+  let container = block.querySelector('.container');
+  return container? Array.from(container.children): null;
+}
+
+function blockValue(block){
+  let input = block.querySelector('input');
+  return input? Number(input.value): null;
+}
+
+function blockUnits(block){
+  if (block.childNodes.length > 1 &&
+      block.lastChild.nodeType == Node.TEXT_NODE) {
+        return block.lastChild.textContent;
+      }
+}
+
+function blockScript(block){
+  let script = [
+    block.dataset.name,
+    blockValue(block)
+  ]
+  contents = blockContents(block);
+  if (contents != null){
+    let inner_script = [];
+    contents.forEach(block => inner_script.push(blockScript(block)))
+    script.push(inner_script)
+  } else {
+    script.push(blockUnits(block))
+  }
+  return script;
+}
